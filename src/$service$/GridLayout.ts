f8dab7e1eef6aeea5f327e59ba39367c4851b0df
unit-404 <conway24b@gmail.com>
2024-10-27 20:08:52 +0700

    build library

--- src/$service$/GridLayout.ts
+++ src/$service$/GridLayout.ts
@@ -1,14 +1,5 @@
-import { setStyleRules, type StyleTuple } from "./StyleRules.js";
-
-//
-export const getCorrectOrientation = () => {
-    let orientationType: string = screen.orientation.type;
-    if (!window.matchMedia("((display-mode: fullscreen) or (display-mode: standalone) or (display-mode: window-controls-overlay))").matches) {
-        if (matchMedia("(orientation: portrait)").matches) {orientationType = orientationType.replace("landscape", "portrait");} else
-            if (matchMedia("(orientation: landscape)").matches) {orientationType = orientationType.replace("portrait", "landscape");};
-    }
-    return orientationType;
-};
+import { getCorrectOrientation } from "../$core$/GridItemUtils";
+import { setStyleRules, type StyleTuple } from "./StyleRules";
 
 //
 export const whenAnyScreenChanges = (cb)=>{
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from "./$core$/GridItemUtils";
+export * from "./$service$/GridLayout";
+
+//
+import "./$scss$/_GridLayout.scss";
