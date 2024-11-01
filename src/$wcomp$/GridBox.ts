e3949c2ff4beed9d9be46f94da8bb48670e4945f
unit-404 <conway24b@gmail.com>
2024-11-01 18:29:19 +0700

    TODO: support compressed styles

--- src/$wcomp$/GridBox.ts
+++ src/$wcomp$/GridBox.ts
@@ -1,5 +1,5 @@
 // @ts-ignore
-import styles from "./GridBox.scss?inline";
+import styles from "./GridBox.scss?inline&compress";
 
 //
 export const preInit = URL.createObjectURL(new Blob([styles], {type: "text/css"}));
@@ -43,12 +43,40 @@ export default () => {};
 export { GridBoxElement };
 
 //
-const loadBlobStyle = (preInit: string)=>{
+const OWNER = "gridbox";
+
+//
+const setStyleURL = (base: [any, any], url: string)=>{
+    //
+    if (base[1] == "innerHTML") {
+        base[0][base[1]] = `@import url("${url}");`;
+    } else {
+        base[0][base[1]] = url;
+    }
+}
+
+//
+const loadStyleSheet = (inline: string, base?: [any, any])=>{
+    const url = URL.canParse(inline) ? inline : URL.createObjectURL(new Blob([inline], {type: "text/css"}));
+    if (base) setStyleURL(base, url);
+}
+
+//
+const loadInlineStyle = (inline: string, rootElement = document.head)=>{
+    const style = document.createElement("style");
+    style.dataset.owner = OWNER;
+    loadStyleSheet(inline, [style, "innerHTML"]);
+    (rootElement.querySelector("head") ?? rootElement).appendChild(style);
+}
+
+//
+const loadBlobStyle = (inline: string)=>{
     const style = document.createElement("link");
     style.rel = "stylesheet";
     style.type = "text/css";
-    style.href = preInit;
-    style.dataset.owner = "grid";
+    style.crossOrigin = "same-origin";
+    style.dataset.owner = OWNER;
+    loadStyleSheet(inline, [style, "href"]);
     document.head.appendChild(style);
     return style;
 }
