fc098a0b36b662167fd27c3871e6d808efd231c9
u2re-dev <conway24b@yandex.ru>
2025-01-02 10:37:23 +0700

    reduce code

--- src/$wcomp$/GridBox.ts
+++ src/$wcomp$/GridBox.ts
@@ -1,8 +1,3 @@
-// @ts-ignore
-import styles from "./GridBox.scss?inline&compress";
-
-//
-export const preInit = URL.createObjectURL(new Blob([styles], {type: "text/css"}));
 
 //
 export class UIGridBoxElement extends HTMLElement {
@@ -32,51 +27,3 @@ export class UIGridBoxElement extends HTMLElement {
 //
 customElements.define("ui-gridbox", UIGridBoxElement);
 export default () => {};
-
-//
-const OWNER = "gridbox";
-
-//
-const setStyleURL = (base: [any, any], url: string)=>{
-    //
-    if (base[1] == "innerHTML") {
-        base[0][base[1]] = `@import url("${url}");`;
-    } else {
-        base[0][base[1]] = url;
-    }
-}
-
-//
-const hash = async (string: string) => {
-    const hashBuffer = await crypto.subtle.digest('SHA-256', new TextEncoder().encode(string));
-    return "sha256-" + btoa(String.fromCharCode.apply(null, new Uint8Array(hashBuffer) as unknown as number[]));
-}
-
-//
-const loadStyleSheet = async (inline: string, base?: [any, any], integrity?: string|Promise<string>)=>{
-    const url = URL.canParse(inline) ? inline : URL.createObjectURL(new Blob([inline], {type: "text/css"}));
-    if (base?.[0] && (!URL.canParse(inline) || integrity) && base?.[0] instanceof HTMLLinkElement) {
-        const I: any = (integrity ?? hash(inline));
-        if (typeof I?.then == "function") {
-            I?.then?.((H)=>base?.[0]?.setAttribute?.("integrity", H));
-        } else {
-            base?.[0]?.setAttribute?.("integrity", I as string);
-        }
-    }
-    if (base) setStyleURL(base, url);
-}
-
-//
-const loadBlobStyle = (inline: string)=>{
-    const style = document.createElement("link");
-    style.rel = "stylesheet";
-    style.type = "text/css";
-    style.crossOrigin = "same-origin";
-    style.dataset.owner = OWNER;
-    loadStyleSheet(inline, [style, "href"]);
-    document.head.appendChild(style);
-    return style;
-}
-
-//
-loadBlobStyle(preInit);
--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,19 @@ export * from "./$core$/Properties";
 export * from "./$wcomp$/GridBox";
 export * from "./$system$/TrackingSystem";
 
-//
+// @ts-ignore
+import styles from "./$wcomp$/GridBox.scss?inline&compress";
+export const preInit = URL.createObjectURL(new Blob([styles], {type: "text/css"}));
+
+// @ts-ignore
+import { loadBlobStyle } from "/externals/lib/dom.js";
 import prop from "./$core$/Properties";
 
 // stub
-const init = ()=>{ prop(); };
+const init = ()=>{
+    loadBlobStyle(preInit);
+    prop();
+};
+
+//
 export default init;
